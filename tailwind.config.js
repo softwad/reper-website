8a7654a0926dd7e9eaf335b76f812c8206371fb8
Sofwad <softwad@gmail.com>
2026-09-22 15:58:57 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../*.html"],
+  theme: {
+    extend: {
+      colors: {
+        brand: { red: '#E3000F', dark: '#1F2937' }
+      }
+    }
+  },
+  plugins: [],
+}
